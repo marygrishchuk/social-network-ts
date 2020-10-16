8eee4ac625a0bd50a7375d94fadb54e737bd27e0
Maria Grishchuk <mary.grishchuk1@gmail.com>
2020-10-16 14:16:29 +0300

    Updates

---
+++ precache-manifest.c5723971eec6b13f99bcf5986b2b853e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0d9049bc37a650e94ea3ccfb24d6ca",
+    "url": "/social-network-ts/index.html"
+  },
+  {
+    "revision": "8cb7e80b08b80028f3c1",
+    "url": "/social-network-ts/static/css/main.0ff464d3.chunk.css"
+  },
+  {
+    "revision": "4cabc7b62694c1566a11",
+    "url": "/social-network-ts/static/js/2.b299a10d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network-ts/static/js/2.b299a10d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cb7e80b08b80028f3c1",
+    "url": "/social-network-ts/static/js/main.1ca55113.chunk.js"
+  },
+  {
+    "revision": "10287e56dae2bbee4b68",
+    "url": "/social-network-ts/static/js/runtime-main.0b8a904f.js"
+  },
+  {
+    "revision": "09fdb778b1fa539853cdd1fdc94b09c9",
+    "url": "/social-network-ts/static/media/logo.09fdb778.jpg"
+  }
+]);
