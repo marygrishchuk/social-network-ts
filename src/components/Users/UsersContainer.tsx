94eb82bdab14b6683d14ca6f2b0fd658dea48fc4
Maria Grishchuk <mary.grishchuk1@gmail.com>
2020-11-30 01:18:48 +0300

    Users and UsersContainer have been changed (56)

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,8 +1,65 @@
 import React from "react";
 import {ActionTypes, RootStateType} from "../../redux/redux-store";
 import {connect} from "react-redux";
+import {
+    followAC,
+    setCurrentPageAC,
+    setTotalUsersCountAC,
+    setUsersAC,
+    unfollowAC,
+    UserType
+} from "../../redux/users-reducer";
 import Users from "./Users";
-import {followAC, setCurrentPageAC, setUsersAC, unfollowAC, setTotalUsersCountAC, UserType} from "../../redux/users-reducer";
+import axios from "axios";
+
+type PropsType = {
+    users: Array<UserType>
+    pageSize: number
+    totalUsersCount: number
+    currentPage: number
+    follow: (userId: string) => void
+    unfollow: (userId: string) => void
+    setUsers: (users: Array<UserType>) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalUsersCount: (totalUsersCount: number) => void
+}
+
+class UsersContainer extends React.Component<PropsType> {
+
+    componentDidMount() {
+        if (this.props.users.length === 0) {
+            axios
+                .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+                .then(response => {
+                    this.props.setUsers(response.data.items)
+                    this.props.setTotalUsersCount(response.data.totalCount)
+                })
+        }
+    }  //'if' condition is needed to avoid duplication of users loaded again
+    // after we switch to another page with Route & return to this page
+    // which will lead to the reconstruction of the object (class instance)
+
+    onPageChanged = (pageNumber: number) => { //arrow syntax is needed to save the call context
+        this.props.setCurrentPage(pageNumber)
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    render() {
+        return <Users
+            users={this.props.users}
+            pageSize={this.props.pageSize}
+            totalUsersCount={this.props.totalUsersCount}
+            currentPage={this.props.currentPage}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+            onPageChanged={this.onPageChanged}/>
+    }
+}
 
 type MapStatePropsType = {
     users: Array<UserType>
@@ -51,7 +108,7 @@ const mapDispatchToProps = (dispatch: (action: ActionTypes) => void): MapDispatc
         }
     }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Users)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
 //shorter than before:
 // const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
 // export default UsersContainer
